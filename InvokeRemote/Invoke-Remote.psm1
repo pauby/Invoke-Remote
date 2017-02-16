f87c43387e3cbd0d4a2b6cbf47cd7c2d209a44f0
Manfred Wallner <schusterfredl@mwallner.net>
2017-02-16 18:54:30 +0100

    initial checkin

---
+++ InvokeRemote/Invoke-Remote.psm1
@@ -0,0 +1,86 @@
+<# 
+
+Invoke-Remote requires 'Powershell Remoting' to be enabled to target hosts.
+> use 'Enable-PSRemoting -Force' on any host you want to connect to
+
+#>
+$ErrorActionPreference = 'Stop' # better be safe than sorry!
+
+
+function Invoke-Remote {
+  param(
+    # ip or hostname of remote host
+    [Parameter(Mandatory=$True)]
+    [string] $ComputerName,
+    # commands to run
+    [Parameter(Mandatory=$False)]
+    [string[]] $commands,
+    # scripts to execute
+    [Parameter(Mandatory=$False)]
+    [string[]] $scripts,
+    # credentials used for login
+    [Parameter(Mandatory=$False)]
+    [SecureString] $credential = $null
+  )
+  
+  # check if remote connection are possible to the correspeding host
+  Test-WsMan $ComputerName
+
+  # the one and only - all commands will be run in this session
+  $remotesession = New-PSSession -computername $ComputerName
+  $resultobj = @{}
+  $resultobj.commands_in = $commands
+  if ($commands) {
+    $resultobj.commands_out = @()
+    $commands | ForEach-Object {
+      $scriptblk = [scriptblock]::Create($_)
+      $resultobj.commands_out += $(Invoke-Command -ScriptBlock $scriptblk -session $remotesession )
+    }
+  }
+  
+  $resultobj.scripts_in = $scripts
+  if ($scripts) {
+    $resultobj.scripts_out = @()
+    $scripts | ForEach-Object {
+      $path = $_
+      $resultobj.scripts_out += $(Invoke-Command -FilePath $path -session $remotesession)
+    }
+  }
+  $resultobj
+}
+Set-Alias -name ir -Value Invoke-Remote
+
+
+function Get-BoxstarterEnv {
+  Import-Module Boxstarter.Bootstrapper -Force -Verbose
+  Import-Module Boxstarter.Chocolatey -Force -Verbose
+  Import-Module Boxstarter.Common -Force -Verbose
+  Import-Module Boxstarter.HyperV -Force -Verbose
+  Import-Module Boxstarter.WinConfig -Force -Verbose
+}
+
+
+function Install-ChocolateyRemote {
+   param(
+    # ip or hostname of remote host
+    [Parameter(Mandatory=$True)]
+    [string] $ComputerName,
+    # chocolatey package to install
+    [Parameter(Mandatory=$True)]
+    [string] $PackageName,
+    # credentials used for login
+    [Parameter(Mandatory=$False)]
+    [SecureString] $credential = $null
+  )
+  Get-BoxstarterEnv
+  if ($credentials) {
+    Install-BoxstarterPackage -ComputerName $ComputerName -PackageName $PackageName -credentials $credential
+  } else {
+    Install-BoxstarterPackage -ComputerName $ComputerName -PackageName $PackageName
+  }
+} 
+
+
+Export-ModuleMember Invoke-Remote
+Export-ModuleMember Install-ChocolateyRemote
+Export-ModuleMember -Alias ir
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Invoke-Remote
 [WIP] enhance workflow when dealing with remote Windows environments
+
+## Basics
+Invoke-Remote ist just a wrapper for `Invoke-Command` and other [Boxstarter](https://github.com/mwrock/boxstarter) commands.
+You'll need [Chocolatey](https://chocolatey.org/) and [Boxstarter](https://github.com/mwrock/boxstarter) all set up to use all of Invoke-Remote's goodness!
+
+## Features
+### Command Invoke-Remote
+Adds some spice to `Invoke-Command`
+
+The result of `Invoke-Remote` is an object with following structure:
+| Member         | Type          | Description                |
+| -------------  | ------------  | -------------------------  |
+| commands_in    | string[]      | the input commands that were run |
+| commands_out   | string[]      | the output of commands_in |
+| scripts_in     | string[]      | the path of the scripts that were run |
+| scripts_out    | string[]      | the output of the scripts in scripts_in |
+
+### Command Install-ChocolateyRemote
+Just a wrapper for `Install-BoxstarterPackage` that ensures you've got Boxstarter fired up.
+
+## Sample Usage
+You'll need to import the module first!
+```
+> Import-Module InvokeRemote\Invoke-Remote.psm1
+```
+
+sending two commands to a host called "some_hostname":
+```
+> Invoke-Remote "some_hostname" "Write-Host Hello World","Write-Host YAY from $(hostname)"
+```
+
+for the lazy sysadmins, there is also an 'ir' Alias :-)
+```
+> ir "some_hostname" "echo 'this is $(hostname)'"
+```
+
+## Roadmap
+what you see currently is a very, very basic module that just provides some helper functions for remote operations.
+
+I plan on adding:
+* own DSL compatible with [psake](https://github.com/psake/psake)
+  * tasks, dependencies, tests
+* support for parallel tasking
+* better error-handling
+* reporting + statistics
