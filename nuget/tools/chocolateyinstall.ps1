4c5258d0b50c97a9a4cf5486b63da5cae5eb1660
Manfred Wallner <schusterfredl@mwallner.net>
2017-06-13 17:13:38 +0200

    improved chocolatey install package

--- nuget/tools/chocolateyinstall.ps1
+++ nuget/tools/chocolateyinstall.ps1
@@ -4,18 +4,28 @@ $packageName = 'Invoke-Remote'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $IRBase = Join-Path $toolsDir "InvokeRemote"
 
-Install-ChocolateyPowershellCommand `
-          -PackageName "$packageName.ir" `
-          -PSFileFullPath $(Join-Path $IRBase "Invoke-Remote.ps1")
+Push-Location $toolsDir
+try {
+  Install-ChocolateyPowershellCommand `
+    -PackageName "$packageName.ir" `
+    -PSFileFullPath $(Join-Path $IRBase "Invoke-Remote.ps1")
+  .\Add-Alias.ps1 -visibility alluser -aliasName "Invoke-Remote" -aliasTarget $(Join-Path $IRBase "Invoke-Remote.ps1")
 
-Install-ChocolateyPowershellCommand `
-          -PackageName "$packageName.ir-psake" `
-          -PSFileFullPath $(Join-Path $IRBase "Invoke-RemotePsake.ps1")
+  Install-ChocolateyPowershellCommand `
+    -PackageName "$packageName.ir-psake" `
+    -PSFileFullPath $(Join-Path $IRBase "Invoke-RemotePsake.ps1")
+  .\Add-Alias.ps1 -visibility alluser -aliasName "Invoke-RemotePsake" -aliasTarget $(Join-Path $IRBase "Invoke-RemotePsake.ps1")
 
-Install-ChocolateyPowershellCommand `
-          -PackageName "$packageName.ir-pester" `
-          -PSFileFullPath $(Join-Path $IRBase "Invoke-RemotePester.ps1")
+  Install-ChocolateyPowershellCommand `
+    -PackageName "$packageName.ir-pester" `
+    -PSFileFullPath $(Join-Path $IRBase "Invoke-RemotePester.ps1")
+  .\Add-Alias.ps1 -visibility alluser -aliasName "Invoke-RemotePester" -aliasTarget $(Join-Path $IRBase "Invoke-RemotePester.ps1")
 
-Install-ChocolateyPowershellCommand `
-          -PackageName "$packageName.ir-choco" `
-          -PSFileFullPath $(Join-Path $IRBase "Install-ChocolateyRemote.ps1")
+  Install-ChocolateyPowershellCommand `
+    -PackageName "$packageName.ir-choco" `
+    -PSFileFullPath $(Join-Path $IRBase "Install-ChocolateyRemote.ps1")
+  .\Add-Alias.ps1 -visibility alluser -aliasName "Invoke-ChocolateyRemote" -aliasTarget $(Join-Path $IRBase "Invoke-ChocolateyRemote.ps1")
+}
+finally {
+  Pop-Location
+}
